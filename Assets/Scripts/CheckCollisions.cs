456d4b8648f377e94182d8429c59ff6199210017
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2023-06-21 03:20:19 +0300

    Feat: add obstacles and finish line
    Add other obstacles, tags and their respective scripts, also add finish line collider, tag and scripts

--- Assets/Scripts/CheckCollisions.cs
+++ Assets/Scripts/CheckCollisions.cs
@@ -7,16 +7,28 @@ public class CheckCollisions : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI CoinText;
+    public PlayerController playerController;
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Coin"))
         {
             //Debug.Log(other.gameObject.name);
             AddCoin();
             //Destroy(other.gameObject);
             other.gameObject.SetActive(false);
+        }
+        else if (other.CompareTag("Finish"))
+        {
+            Debug.Log("Congrats!");
+            playerController.runningSpeed = 0;
+        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Found Obstacle!!");
         }
     }
 
