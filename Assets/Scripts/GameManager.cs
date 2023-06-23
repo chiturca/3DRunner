cd1d5ae3f2bff2f183cd00ceb29da45d97b846b0
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2023-06-23 18:01:43 +0300

    Feat: count in game ranking on UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private InGameRanking ig;
+
+    private GameObject[] runners;
+    List<RankingSystem> sortArray = new List<RankingSystem>();
+
+    private void Awake()
+    {
+        instance = this;
+        runners = GameObject.FindGameObjectsWithTag("Runner");
+        ig = FindObjectOfType<InGameRanking>();
+    }
+    void Start()
+    {
+        for (int i = 0; i < runners.Length; i++)
+        {
+            sortArray.Add(runners[i].GetComponent<RankingSystem>());
+        }
+    }
+
+    void CalculateRank()
+    {
+        sortArray = sortArray.OrderBy(x => x.distance).ToList();
+        sortArray[0].rank = 1;
+        sortArray[1].rank = 2;
+        sortArray[2].rank = 3;
+        sortArray[3].rank = 4;
+        sortArray[4].rank = 5;
+        sortArray[5].rank = 6;
+        sortArray[6].rank = 7;
+
+        ig.a = sortArray[6].name;
+        ig.b = sortArray[5].name;
+        ig.c = sortArray[4].name;
+        ig.d = sortArray[3].name;
+        ig.e = sortArray[2].name;
+        ig.f = sortArray[1].name;
+        ig.g = sortArray[0].name;
+    }
+
+    void Update()
+    {
+        CalculateRank();
+    }
+}
---
+++ Assets/Scripts/RankingSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingSystem : MonoBehaviour
+{
+    public float distance;
+    public GameObject target;
+    public int rank;
+    
+
+    void CalculateDistance()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        CalculateDistance();
+    }
+}
